745efcbffc90e183412d030d55df44df0f779a05
Serhii Pylypchuk (DS) <serhiipylypchuk@Serhiis-MacBook-Pro.local>
2024-07-16 10:34:32 +0400

    First commit

---
+++ CS/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using DevExpress.Maui.Core;
+using DevExpress.Maui.Editors;
+using Microsoft.Maui.Controls;
+
+namespace ThemeView; 
+
+public partial class ThemesPage : ContentPage {
+    public ThemesPage() {
+        InitializeComponent();
+    }
+}
---
+++ CS/ViewModels/ThemesViewModel.cs
@@ -0,0 +1,95 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+using DevExpress.Maui.Core;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace ThemeSelectorApp;
+
+public partial class ThemesViewModel : ObservableObject {
+    [ObservableProperty]
+    ObservableCollection<ColorModel> items;
+
+    [ObservableProperty]
+    ColorModel selectedColor;
+
+    [ObservableProperty]
+    Color previewColor;
+
+    [ObservableProperty]
+    string previewColorName;
+
+    [ObservableProperty]
+    public List<string> themeTypes = new List<string>() { "System", "Dark", "Light" };
+
+    [ObservableProperty]
+    public string selectedThemeType;
+
+    partial void OnSelectedThemeTypeChanged(string value) {
+        switch (value) {
+            case "Light": {
+                Application.Current.UserAppTheme = AppTheme.Light;
+                break;
+            }
+            case "Dark": {
+                Application.Current.UserAppTheme = AppTheme.Dark;
+                break;
+            }
+            case "System": {
+                Application.Current.UserAppTheme = AppTheme.Unspecified;
+                break;
+            }
+        }
+    }
+
+    partial void OnSelectedColorChanged(ColorModel value) {
+        if (value == null)
+            return;
+
+        PreviewColorName = value.DisplayName;
+        if (value.IsSystemColor) {
+            ThemeManager.UseAndroidSystemColor = true;
+            return;
+        }
+        
+        ThemeManager.UseAndroidSystemColor = false;
+        ThemeManager.Theme = new Theme(value.Color);
+    }
+
+    public ThemesViewModel() {
+        Items = new ObservableCollection<ColorModel>() {
+#if ANDROID
+            new ColorModel(Colors.Black, "System Color", true),
+#endif
+            new ColorModel(ThemeManager.GetSeedColor(ThemeSeedColor.Purple), ThemeSeedColor.Purple.ToString()),
+            new ColorModel(ThemeManager.GetSeedColor(ThemeSeedColor.Violet), ThemeSeedColor.Violet.ToString()),
+            new ColorModel(ThemeManager.GetSeedColor(ThemeSeedColor.Red), ThemeSeedColor.Red.ToString()),
+            new ColorModel(ThemeManager.GetSeedColor(ThemeSeedColor.Brown), ThemeSeedColor.Brown.ToString()),
+            new ColorModel(ThemeManager.GetSeedColor(ThemeSeedColor.TealGreen), ThemeSeedColor.TealGreen.ToString()),
+            new ColorModel(ThemeManager.GetSeedColor(ThemeSeedColor.Green), ThemeSeedColor.Green.ToString()),
+            new ColorModel(ThemeManager.GetSeedColor(ThemeSeedColor.DarkGreen), ThemeSeedColor.DarkGreen.ToString()),
+            new ColorModel(ThemeManager.GetSeedColor(ThemeSeedColor.DarkCyan), ThemeSeedColor.DarkCyan.ToString()),
+            new ColorModel(ThemeManager.GetSeedColor(ThemeSeedColor.DeepSeaBlue), ThemeSeedColor.DeepSeaBlue.ToString()),
+            new ColorModel(ThemeManager.GetSeedColor(ThemeSeedColor.Blue), ThemeSeedColor.Blue.ToString()),
+        };
+        SelectedColor = Items[0];
+        SelectedThemeType = ThemeTypes[0];
+    }
+}
+
+public class ColorModel {
+    public Color Color { get; set; }
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+    public bool IsSystemColor { get; set; }
+
+    public ColorModel(Color color, string displayName, bool isSystemColor = false) {
+        Color = color;
+        DisplayName = displayName;
+        IsSystemColor = isSystemColor;
+        Name = isSystemColor ? "System" : string.Empty;
+    }
+}
